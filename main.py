5900d5e6a22e506b060606bace18306e19a14b44
= <kittholland@gmail.com>
2023-04-07 09:04:05 -0700

    Added a few sample queries, switched to map_reduce chain

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os, yaml, pinecone, tiktoken
+import yaml, pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Pinecone
 from langchain.chains import RetrievalQA
@@ -13,7 +13,9 @@ pinecone_environment = config['pinecone_environment']
 pinecone.init(environment=pinecone_environment)
 docsearch = Pinecone.from_existing_index(pinecone_index_name, OpenAIEmbeddings())
 
-qa_chain = load_qa_chain(OpenAI(temperature=0.8), chain_type="stuff", verbose=True)
+qa_chain = load_qa_chain(OpenAI(temperature=0.8), chain_type="map_reduce", verbose=True)
 qa = RetrievalQA(combine_documents_chain=qa_chain, retriever=docsearch.as_retriever(), verbose=True)
-query = "What is Bartenwerfer v. Buckley?"
+#query = "What is ARELLANO v. McDONOUGH?"
+#query = "Who is REBECCA A. WOMELDORF?"
+query = "Which supreme court justice wrote the concurring opinion in BARTENWERFER v. BUCKLEY?"
 qa.run(query)
